260abe399486a42a1007787024d9ca8aa5fb66f5
Malcolm Maturen <malc@umich.edu>
2017-11-26 21:56:44 -0500

    Updated ideas

--- Maturen206Final.py
+++ Maturen206Final.py
@@ -1,1 +1,3 @@
-# malcolm maturen -- malc
+# name: malcolm maturen -- malc
+# ideas: measures popuarity of reddit post based on time of day
+# OR creates network visualization of political sentiment and user interaction
